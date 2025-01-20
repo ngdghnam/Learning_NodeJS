0743854d747621aa0e7ea526ac86b563db7ac367
Nam <stghoainam4002@gmail.com>
2025-01-20 17:11:08 +0700

    Finished

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
 
-// Set some defaults
-db.defaults({ users: [] }).write();
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [], sessions: [], transfers: [] }).write();
+db.defaults({ products: [] }).write();
+
 
 module.exports = db;
